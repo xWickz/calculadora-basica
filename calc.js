6b1a3dbcd24631bb9c351822582cb55dfe961c44
xWickz <wickzcs@gmail.com>
2025-03-27 07:20:23 -0400

    fix: decimal

--- calc.js
+++ calc.js
@@ -1,6 +1,14 @@
 let num1 = "";
 let num2 = "";
 let operador = "";
+
+const state = {
+  num1: "",
+  num2: "",
+  operador: "",
+};
+
+const operadores = ["+","-","x","/"];
 const calculadora = document.getElementById("resultado");
 
 /* Funciones Principales */
@@ -16,9 +24,9 @@ function calcular(n1, n2, op) {
 
   if(isNaN(n1) || isNaN(n2)) {
     calculadora.value = "Entrada inválida";
-    num1 = "";
-    num2 = "";
-    operador = "";
+    state.num1 = "";
+    state.num2 = "";
+    state.operador = "";
     return;
   }
 
@@ -48,95 +56,70 @@ function calcular(n1, n2, op) {
 
 function agregarNumero(numero) {
   console.log(numero);
-  if (operador === "") {
-    num1 += numero;
-    calculadora.value = num1;
+  if (state.operador === "") {
+    state.num1 += numero;
+    calculadora.value = state.num1;
   } else {
-    num2 += numero;
-    calculadora.value = `${num1} ${operador} ${num2}`;
+    state.num2 += numero;
+    calculadora.value = `${state.num1} ${state.operador} ${state.num2}`;
   }
 }
 
 function agregarOperacion(operacion) {
-  if (num1 === "") return;
+  if (state.num1 === "") return;
 
   /* Operaciones en Automático */
-  if (operador !== "") {
+  if (state.operador !== "") {
     ejecutarCalculo();
   }
 
-    if(!["+","-","x","/"].includes(calculadora.value.trim().slice(-1))) {
-      operador = operacion;
-      calculadora.value += " " + operador;
+    if(!operadores.includes(calculadora.value.trim().slice(-1))) {
+      state.operador = operacion;
+      calculadora.value += " " + state.operador;
   }
+
 }
 
 function ejecutarCalculo() {
-  const resultado = calcular(num1, num2, operador);
+  const resultado = calcular(state.num1, state.num2, state.operador);
   if(resultado !== undefined) {
     calculadora.value = resultado;
-    num1 = resultado.toString();
-    num2 = "";
-    operador = "";
+    state.num1 = resultado.toString();
+    state.num2 = "";
+    state.operador = "";
   }
 }
 
 function limpiar() {
-  num1 = "";
-  num2 = "";
-  operador = "";
+  state.num1 = "";
+  state.num2 = "";
+  state.operador = "";
   calculadora.value = "";
   resultado = "";
 }
 
 /* Funciones Adicionales */
-function decimal() { // Función Experimental (Lo digo por los console-log).
-  
-  if(calculadora.value === "") { // Si la calculadora esta vacía, entonces se pone "0." en vez de "." al primer número.
-    num1 = "0.";
-    return;
-  }
-
-  if(resultado !== "") {
-    if(!num1.includes(".") && operador === "") { 
-      // Si el número 1 tiene valor, pero, no tiene ".", el operador es vacío y no hay resultado, aún.
-      num1 += ".";
-      return;
-    } else if(num1 === "") { 
-      // Si en este caso el número 1 no contiene valor se le antepone "0."
-      num1 += "0.";
-      return;
+function decimal() {
+  if(state.operador === "") {
+    if(!state.num1.includes(".")) {
+      state.num1 += state.num1 === "" ? "0." : ".";
     }
-  }
-
-  if(!num1.includes(".") && resultado === "") {
-    // Si el número 1 tiene valor pero no tiene ".". Adicional, el resultado es vacío.
-    if(num1 === "") {
-        // Si el número 1 no tiene valor, se le antepone un "0."
-      num1 = "0.";
-      return;
+  } else {
+    if(!state.num2.includes(".")) {
+      state.num2 += state.num2 === "" ? "0." : ".";
     }
-      num1 += ".";
-      return;
-  } else if(!num2.includes(".") && operador !== "") {
-    if(num2 === "") {
-      num2 += "0.";
-      return;
-    }
-      num2 += ".";
-      return;
   }
 }
 
 function factorial() {
   if (calculadora.value === "") return;
 
-  if (!num1.includes("!")) {
-    num1 += "!";
+  if (!state.num1.includes("!")) {
+    state.num1 += "!";
     calculadora.value += "!";
     return;
-  } else if (!num2.includes("!") && num2 !== "") {
-    num2 += "!";
+  } else if (!state.num2.includes("!") && state.num2 !== "") {
+    state.num2 += "!";
     calculadora.value += "!";
     return;
   }
@@ -171,7 +154,7 @@ function calcularFactorial(n) {
 document.addEventListener("keydown", function (event) {
   if (event.key >= "0" && event.key <= "9") {
     agregarNumero(event.key);
-  } else if (["*", "-", "+", "/"].includes(event.key)) {
+  } else if (operadores.includes(event.key)) {
     agregarOperacion(event.key);
   } else if (event.key === "Enter") {
     ejecutarCalculo();
